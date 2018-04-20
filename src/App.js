7e5f2cb2d8109c4d403120c9331f4cde21326c6b
Jesus Urrutia <jesus.urrutia@gmail.com>
2018-04-20 16:02:38 -0300

    Minor change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">:) Welcome (editado) :) to React :) :) :D :Dand Deploy from Github :)</h1>
+          <h1 className="App-title">:) Welcome (editadooooo) :) to React :) :) :D :Dand Deploy from Github :)</h1>
         </header>
         <p className="App-intro">
           To :)get :) :)started :), edit :) :) <code>src/App.js</code> and save to reload.
